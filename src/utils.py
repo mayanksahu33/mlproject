2b43d70840764f0954c49810e4d837c7ce050ab2
Mayank Sahu <sahumayank.18@email.com>
2023-05-16 18:29:05 +0530

    modified templates

--- src/utils.py
+++ src/utils.py
@@ -45,7 +45,7 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,params):
         return report
     
     except Exception as e:
-        CustomException(e,sys)
+        raise CustomException(e,sys)
 
 
 def load_object(file_path):
